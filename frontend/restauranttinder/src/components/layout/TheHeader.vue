7e32de076b0ef9dcdf838898b2c35b27d1d7ecd4
Becki Robards <becki.robards@gmail.com>
2019-04-10 12:17:18 -0400

    Began Logout button implementation in header

--- frontend/restauranttinder/src/components/layout/TheHeader.vue
+++ frontend/restauranttinder/src/components/layout/TheHeader.vue
@@ -1,10 +1,43 @@
 <template>
-    
+    <header>
+    <div>
+    <nav>
+    <ul>
+        <li v-if="isAuthenticated">
+            <a href="/logout" v-on:click.prevent="logout">Logout</a>
+        </li>
+        <li v-else>
+            <router-link to="/login">Login</router-link>
+        </li>
+    </ul>
+    </nav>
+    </div>
+    </header>
 </template>
 
 <script>
 export default {
-
+  name: "the-header",
+data() {
+    return {
+        isAuthenticated: getUser() !== null
+    };
+},
+methods: {
+    /**
+     * Logs the user out and takes them to the login page
+     * @function
+     */
+    logout() {
+      auth.destroyToken();
+      this.$router.push("/login");
+    }
+},
+computed: {
+    getUser() {
+        return auth.getUser();
+    }
+}
 }
 </script>
 
