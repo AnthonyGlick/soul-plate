1a1c34fa9a89467a3d3549edb7d7ad870534bcc3
Anthony Glick <anthonymglick@gmail.com>
2019-04-12 15:48:48 -0400

    added inputs on home

--- frontend/restauranttinder/src/views/Home.vue
+++ frontend/restauranttinder/src/views/Home.vue
@@ -3,7 +3,12 @@
     <h1>Home</h1>
     <form>
       <auto-complete name="Cuisine" placeholder="Cuisine" id="Cuisine" :choices="cuisineNames"></auto-complete>
-      <google-map/>
+      <section>
+      <input type="checkbox" @change="toggleCoords"/>
+      <span>Check for current location, uncheck for saved address</span>
+      </section>
+      <input type="hidden" id="lat" :value="latToggle"/>
+      <input type="hidden" id="lon" :value="lonToggle"/>
     </form>
   </div>
 </template>
@@ -11,6 +16,7 @@
 <script>
 import AutoComplete from "@/components/query/AutoComplete.vue";
 import GoogleMap from "@/components/maps/GoogleMap.vue";
+import auth from "@/shared/auth";
 export default {
   name: "home",
   components: {
@@ -21,12 +27,45 @@ export default {
     return {
       cuisines: [],
       cuisineNames: [],
-      cuiElement: Object
+      currentUser: {
+        username: auth.getUser().sub,
+        address1: "",
+        city: "",
+        state: "",
+        country: USA
+      },
+      currentCoords: {
+        lat: "",
+        long: ""
+      },
+      addressCoords: {
+        lat: "",
+        long: ""
+      }
     };
   },
-  
+  method: {
+    toggleCoords(){
+      this.latToggle();
+      this.lonToggle();
+    },
+    latToggle(){
+      if (document.getElementById("lat").value == this.currentCoords.lat){
+        document.getElementById("lat").value = this.addressCoords.lat;
+      } else {
+        document.getElementById("lat").value = this.currentCoords.lat;
+      }
+    },
+    lonToggle(){
+      if (document.getElementById("lon").value == this.currentCoords.lon){
+        document.getElementById("lon").value = this.addressCoords.lon;
+      } else {
+        document.getElementById("lon").value = this.currentCoords.lon;
+      }
+    }
+
+  },
   created() {
-    //this.cuiElement = document.getElementById("Cuisine");
     fetch(`${process.env.VUE_APP_ZOMATO_API}/cuisines?city_id=1033`, {
       method: "GET",
       headers: {
@@ -44,6 +83,11 @@ export default {
       .catch(error => console.error(error));
   }
 };
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+}
 </script>
 
 <style>
