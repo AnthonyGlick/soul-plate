8366da4d1c35d8c00a9e2936fb71b179d9cd9a3e
Mariah Dawson <mlynndawson@gmail.com>
2019-04-15 14:08:19 -0400

    working on css for the summary components

--- frontend/restauranttinder/src/components/Home/RestaurantSummary.vue
+++ frontend/restauranttinder/src/components/Home/RestaurantSummary.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="restaurant-summaries">
-    <div class="summary" v-for="restaurant in summaries.restaurants" :key="restaurant.restaurant.id">
-      <div class="featuredimage">
-          <img :src ="restaurant.restaurant.featured_image">
+    <div
+      class="summary"
+      v-for="restaurant in summaries.restaurants"
+      :key="restaurant.restaurant.id"
+    >
+      <div v-if="restaurant.restaurant.featured_image" class="featuredimage">
+        <img :src="restaurant.restaurant.featured_image">
       </div>
+      <div v-else><img class="featuredimage" src= "https://via.placeholder.com/1200x464"></div>
 
-    <div class="text-infor">    
+      <div class="text-infor">
         <h3 class="name">{{restaurant.restaurant.name}}</h3>
-        <h3 class="rating">{{restaurant.restaurant.user_rating.aggregate_rating}}</h3>
-        <h3 class="location">{{restaurant.restaurant.location.locality}}</h3>
+        <h3 class="rating">Rating: {{restaurant.restaurant.user_rating.aggregate_rating}}</h3>
+        <h3 class="location">Location: {{restaurant.restaurant.location.locality}}</h3>
         <h3 class="cuisines">{{restaurant.restaurant.cuisines}}</h3>
         <h3 class="price-range">{{restaurant.restaurant.price_range}}</h3>
-      </div> 
+      </div>
     </div>
   </div>
 </template>
@@ -29,16 +34,19 @@ export default {
   },
   created() {
     fetch(this.testurl, {
-      method: 'GET',
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
         "user-key": `${process.env.VUE_APP_ZOMATO_KEY}`
       }
-    }).then( (response) => {
-      return response.json();
-    }).then( (json) => {
-      this.summaries = json;
-    }).catch( (error) => console.error(error));
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        this.summaries = json;
+      })
+      .catch(error => console.error(error));
   }
   //TODO // methods:{
   //        removeReviewFromArray(id) {
@@ -52,4 +60,12 @@ export default {
 </script>
 
 <style>
+
+.summary{
+  border: 1px solid black;
+  display:flex;
+  flex-direction: column;
+}
+
+
 </style>
