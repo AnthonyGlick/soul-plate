ac18fc358cc8a8c8b394013fba4bea980503250f
Mariah Dawson <mlynndawson@gmail.com>
2019-04-16 11:19:09 -0400

    added working reject button

--- frontend/restauranttinder/src/components/Home/RejectButton.vue
+++ frontend/restauranttinder/src/components/Home/RejectButton.vue
@@ -1,20 +1,17 @@
 <template>
     <div class="reject-button">
-        <button class="btn btn-primary">Reject</button>
+        <button class="btn btn-primary" v-on:click="RejectRestaurant">Reject</button>
     </div>
 </template>
 
 <script>
 export default {
-
-  //TODO // methods:{
-  //        removeReviewFromArray(id) {
-  //       const reviewsMinusDeleted = this.reviews.filter((review) => {
-  //         return review.id !== id;
-  //       });
-  //       this.reviews = reviewsMinusDeleted;
-  // },
-  // }
+    name: "RejectButton",
+    methods:{
+        RejectRestaurant(){
+            this.$emit('Reject')
+        }
+    }
 }
 
 </script>
