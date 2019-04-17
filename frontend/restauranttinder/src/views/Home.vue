355ba7c4e1d54bd0280095048807eeb594015c70
Anthony Glick <anthonymglick@gmail.com>
2019-04-17 17:40:31 -0400

    added swiping functionality

--- frontend/restauranttinder/src/views/Home.vue
+++ frontend/restauranttinder/src/views/Home.vue
@@ -1,51 +1,53 @@
 <template>
- <div id="Home">
-   <restaurant-search
-      v-on:search-results="performSearch"
-   />
-   <restaurant-summary
-       v-bind:summaries="summaries"
-   />
- </div>
+  <v-touch @swipeleft="rejectRestaurant">
+    <v-touch @swiperight="likeRestaurant">
+  <div id="Home">
+    <restaurant-search v-on:search-results="performSearch"/>
+    <restaurant-summary v-bind:summaries="summaries"/>
+  </div>
+    </v-touch>
+  </v-touch>
 </template>
 
 <script>
-import RestaurantSearch from "@/components/Home/RestaurantSearch.vue"
-import RestaurantSummary from "@/components/Home/RestaurantSummary.vue"
-import RejectButton from "@/components/Home/RejectButton.vue"
+import RestaurantSearch from "@/components/Home/RestaurantSearch.vue";
+import RestaurantSummary from "@/components/Home/RestaurantSummary.vue";
+import {bus} from '@/main.js'
 
 export default {
   name: "Home",
   components: {
     RestaurantSearch,
-    RestaurantSummary,
-    RejectButton
+    RestaurantSummary
   },
 
-  data(){
+  data() {
     return {
       summaries: []
+    };
+  },
+  methods: {
+    performSearch(summaries) {
+      this.summaries = summaries;
+    },
+    rejectRestaurant() {
+      bus.$emit('rejectSwipe')
+    },
+    likeRestaurant() {
+      bus.$emit('likeSwipe')
     }
-  },
-methods: {
-  performSearch(summaries) {
-    this.summaries = summaries;
-  },
-
-}
-}
+  }
+};
 </script>
 
 <style>
-
 h1 {
-  font-family: 'Vollkorn', sans-serif;
+  font-family: "Vollkorn", sans-serif;
   padding: 8px;
 }
 
 span {
-  font-family: 'Vollkorn', sans-serif;
+  font-family: "Vollkorn", sans-serif;
   font-size: 18px;
 }
-
 </style>
